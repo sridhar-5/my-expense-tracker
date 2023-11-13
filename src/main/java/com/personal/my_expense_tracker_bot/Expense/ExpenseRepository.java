6087152c9261d875ddf13ffe5fb197329a5b1702
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2023-11-13 18:38:33 +0530

    added expense report logic, yet to test

--- src/main/java/com/personal/my_expense_tracker_bot/Expense/ExpenseRepository.java
+++ src/main/java/com/personal/my_expense_tracker_bot/Expense/ExpenseRepository.java
@@ -2,5 +2,8 @@ package com.personal.my_expense_tracker_bot.Expense;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ExpenseRepository extends CrudRepository<Expense, Long> {
+    List<Expense> findAllByMonthAndYear(int month, int year);
 }
--- src/main/java/com/personal/my_expense_tracker_bot/Expense/ExpenseService.java
+++ src/main/java/com/personal/my_expense_tracker_bot/Expense/ExpenseService.java
@@ -3,7 +3,11 @@ package com.personal.my_expense_tracker_bot.Expense;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ExpenseService {
@@ -18,7 +22,35 @@ public class ExpenseService {
         return expenseRepository.save(expense);
     }
 
-    public List<Expense> createExpenseReport(String month, String year){
+    public ArrayList<Expense> createExpenseReport(String month, String year){
+        return new ArrayList<>(expenseRepository.findAllByMonthAndYear(getMonth(month), getYear(year)));
+    }
 
+
+    public static int getYear(String year){
+        return Integer.parseInt("20"+year);
+    }
+    public static int getMonth(String month){
+        Map<String, Integer> months = createMonthCodeMap();
+        return months.get(month);
+    }
+
+    public static Map<String, Integer> createMonthCodeMap() {
+        Map<String, Integer> monthCodeMap = new HashMap<>();
+
+        monthCodeMap.put("jan", 1);
+        monthCodeMap.put("feb", 2);
+        monthCodeMap.put("mar", 3);
+        monthCodeMap.put("apr", 4);
+        monthCodeMap.put("may", 5);
+        monthCodeMap.put("jun", 6);
+        monthCodeMap.put("jul", 7);
+        monthCodeMap.put("aug", 8);
+        monthCodeMap.put("sep", 9);
+        monthCodeMap.put("oct", 10);
+        monthCodeMap.put("nov", 11);
+        monthCodeMap.put("dec", 12);
+
+        return monthCodeMap;
     }
 }
